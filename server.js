065b3f3b0f518a12d4d5e990ca32794533ca5333
Anoop Mukartihal <mukartihal32@gmail.com>
2023-08-15 21:20:02 +0530

    first

--- server.js
+++ server.js
@@ -1,28 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = '/calculator'
+const baseUrl = "/calculator";
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
-baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+baseRouter.get("/greeting", (req, res) => {
+  console.log("Hello World!");
+  return res.status(200);
 });
 
-baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/add", (req, res) => {
+  var f = req.body.first;
+  var s = req.body.second;
+
+  var ans = f + s;
+
+  res.json({ result: ans });
 });
 
+baseRouter.post("/subtract", (req, res) => {
+  var f1 = req.body.first;
+  var s1 = req.body.second;
 
-baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+  var ans1 = f - s;
+  res.json({ result: ans1 });
 });
 
 app.use(baseUrl, baseRouter);
 app.listen(PORT, () => {
-    console.log("Server running at PORT", PORT);
+  console.log("Server running at PORT", PORT);
 });
